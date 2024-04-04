7badc364c5e148cd652aac249f31bb691550eadc
ash8282khan <149650566+ash8282khan@users.noreply.github.com>
2024-04-04 21:46:01 +0530

    Add files via upload

---
+++ day3/script.js
@@ -0,0 +1,50 @@
+console.log("hello");
+
+// var massage ="hello world";
+// console.log(massage);
+
+// let massage ="hello world";
+// console.log(massage);
+
+// console.log(massage);
+// var massage ="hello world";
+
+//function env(){
+//     console.log("hello");
+
+// var massage ="hello world";
+// console.log(massage);
+
+
+// console.log(massage);
+// var massage ="hello world";
+
+//}
+   
+// const massage ="hello world";
+// console.log(massage);
+
+// function print(str='nothing is here'){
+//     console.log(str);
+// }
+
+//print("hello1");
+
+// function sum(a,b){
+//     if(a&&b){
+//         console.log(a+b);
+//     }
+//     else{
+//         console.log(a);
+//     }
+// }
+
+// sum(22,22);
+// sum(23)
+const obj={
+    firstName:'ashraf',
+    lastName:'khan',
+}
+
+const x = prompt("Enter you first name");
+console.log(obj[x]);
