707b472da46dc5f94f333621f6b36ba1cbcb35c1
ash8282khan <149650566+ash8282khan@users.noreply.github.com>
2024-04-09 16:17:55 +0530

    Add files via upload

---
+++ day6/script.js
@@ -0,0 +1,102 @@
+// console.log("Start");
+
+// setTimeout((test) => { 
+//     console.log("timeOut...")
+// }, 5000);
+// console.log("End");
+
+
+//-------------------------------------------------------
+
+// setTimeout((test) => { 
+//     console.log("timeOut 1...")
+// }, 10000);
+// console.log("Start");
+
+// setTimeout((test) => { 
+//     console.log("timeOut 2...")
+// }, 5000);
+
+// console.log("Intermediate");
+// console.log("End");
+
+//------------------------------------------------------------------
+
+// //createOrder() and makePayment()
+
+// function createOrder(x,fn){
+//     console.log(x);
+
+// }
+
+// function makePayment(orderId){
+//     console.log(orderId);
+// }
+
+// createOrder("zxc",makePayment("qwer4311"));
+
+//------------------ // Create New promise //----------------------------------------------------
+
+
+// const pr = new Promise((resolve, reject) => {
+//     //logic
+//     setTimeout(() => {
+//         // resolve("done-123");
+//         reject("Some items are out of stock");
+//     }, 0);
+// });
+// setTimeout((test) => {
+//     console.log("timeOut 1...")
+// }, 0);
+
+// // console.log(pr);
+
+// pr.then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+//----------------------------------------------------------------------------------
+
+// setTimeout(function abc(){
+//     console.log("i am  1.");
+// }, 0);
+
+// const pr1 = new Promise ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
