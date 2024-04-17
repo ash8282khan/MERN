862c60c7ac563d0415afe665b02ba35350dd4715
ash8282khan <149650566+ash8282khan@users.noreply.github.com>
2024-04-17 22:08:45 +0530

    Delete day10/controllers directory

--- day10/controllers/productsBackend/routes/productsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const productsController = require('../controllers/productsController.js')
-const productRouter = express.Router();
-
-productRouter.route('/')
-    .get(productsController.getAllProducts);
-
-    module.exports = productRouter;
